6e102a1814b1b746716f54b6905e5dcfdcacb0ec
Rods <rodolfo.perez01@iest.edu.mx>
2023-03-24 11:09:20 -0600

    apis y html

---
+++ Js/api.js
@@ -0,0 +1,39 @@
+const getPost = async () => {
+  let response = await fetch(
+    `https://devto-9f944-default-rtdb.firebaseio.com/.json`
+  );
+
+  let data = response.json();
+
+  return data;
+};
+
+const getPostId = async (id) => {
+  let response = await fetch(
+    `https://devto-9f944-default-rtdb.firebaseio.com/${id}/.json`
+  );
+
+  let data = response.json();
+
+  return data;
+};
+
+const createPost = async (postInfo) => {
+  let response = await fetch(
+    `https://devto-9f944-default-rtdb.firebaseio.com/data/.json`,
+    { method: "POST", body: JSON.stringify(postInfo) }
+  );
+
+  let data = response.json();
+  return data;
+};
+
+const deletePost = async (id) => {
+  let response = await fetch(
+    `https://devto-9f944-default-rtdb.firebaseio.com/${id}/.json`,
+    { method: "DELETE" }
+  );
+
+  let data = response.json();
+  return data;
+};
